cb94761baa49f495fe0561fe7ccac9f1e2f7a20e
011223345aA <huyhoang11202@gmail.com>
2023-06-07 21:17:01 +0700

    add category ajax

--- src/main/java/h2tk/ecom/controller/CategoryController.java
+++ src/main/java/h2tk/ecom/controller/CategoryController.java
@@ -6,9 +6,7 @@ import h2tk.ecom.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +22,8 @@ public class CategoryController {
         return CateRepo.findAll();
     }
 
+    @PostMapping("/saveCategory")
+    public Categories Addcategory(@RequestBody Categories category){
+        return CateRepo.save(category);
+    }
 }
--- src/main/resources/static/ajax/categories.js
+++ src/main/resources/static/ajax/categories.js
@@ -1,5 +1,27 @@
 $(document).ready(()=>{
    loadCategory();
+
+   $("#frmCategory").submit((event)=>{
+       event.preventDefault();
+       let form = $(this);
+       $.ajax({
+          url: "/api/saveCategory",
+           method:"POST",
+          contentType: "application/json",
+           data: JSON.stringify({
+                name : $("#nameCategory").val(),
+           }),
+           success: (response) =>{
+               alert("Category added successfully");
+               form.trigger("reset");
+               loadCategory();
+           },
+           error:()=>{
+               alert("Category added Failed");
+           },
+
+       });
+   });
 });
 
 function loadCategory(){
